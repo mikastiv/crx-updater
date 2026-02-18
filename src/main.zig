8ea920e9a364c1358de9be5940788137f8cded43
mikastiv <mikastiv@outlook.com>
2026-02-18 02:47:49 -0400

    print extension name

--- src/main.zig
+++ src/main.zig
@@ -8,55 +8,131 @@ var stderr_buffer: [1024]u8 = undefined;
 var stderr_writer = std.fs.File.stderr().writer(&stderr_buffer);
 const stderr = &stderr_writer.interface;
 
+const manifest_json = "manifest.json";
+
 pub fn main() !void {
     var arena: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
     defer arena.deinit();
 
     const allocator = arena.allocator();
 
-    const id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
+    const ids: []const []const u8 = &.{
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm",
+        "aeblfdkhhhdcdjpifhhbdiojplfjncoa",
+        "eimadpbcbfnmbkopoojfekhnkhdbieeh",
+        "ldpochfccmkkmhdbclfhpagapcfdljkj",
+        "pkehgijcmpdhfbdbbnkijodmdjhbjlgp",
+        "ponfpcnoihfmfllpaingbgckeeldkhle",
+    };
 
-    const chrome_result = try std.process.Child.run(.{
-        .allocator = allocator,
-        .argv = &.{ "chromium", "--version" },
-    });
-    const browser_version = getChromeVersion(chrome_result.stdout);
+    for (ids) |id| {
+        const browser_version = try getChromeVersion(allocator);
 
-    const filename = try makeTempName(allocator);
+        const filename = try makeTempName(allocator);
 
-    const zip_archive = try downloadCrxFile(allocator, filename, browser_version, id);
-    defer std.fs.deleteFileAbsolute(filename) catch {};
-    defer zip_archive.close();
+        const zip_archive = try downloadCrxFile(allocator, filename, browser_version, id);
+        defer std.fs.deleteFileAbsolute(filename) catch {};
+        defer zip_archive.close();
 
-    var zip_buffer: [1024]u8 = undefined;
-    var zip_reader = zip_archive.reader(&zip_buffer);
+        var zip_buffer: [1024]u8 = undefined;
+        var zip_reader = zip_archive.reader(&zip_buffer);
 
-    const tempdir = try makeTempName(allocator);
-    try std.fs.makeDirAbsolute(tempdir);
-    defer std.fs.deleteTreeAbsolute(tempdir) catch {};
+        const tempdir = try makeTempName(allocator);
+        try std.fs.makeDirAbsolute(tempdir);
+        defer std.fs.deleteTreeAbsolute(tempdir) catch {};
 
-    var dest = try std.fs.openDirAbsolute(tempdir, .{});
-    defer dest.close();
+        var dest = try std.fs.openDirAbsolute(tempdir, .{});
+        defer dest.close();
 
-    try std.zip.extract(dest, &zip_reader, .{});
+        const locale = try extractManifestAndLocale(allocator, &zip_reader, dest);
 
-    const manifest = try dest.openFile("manifest.json", .{});
-    defer manifest.close();
+        const manifest = try dest.openFile(manifest_json, .{});
+        defer manifest.close();
 
+        const root = try parseJsonFile(allocator, manifest);
+
+        var extension_name = if (root.object.get("name")) |name| name.string else "unknown";
+        if (std.mem.startsWith(u8, extension_name, "__MSG")) {
+            if (try lookupLocaleName(allocator, dest, locale.?, extension_name)) |name| {
+                extension_name = name;
+            }
+        }
+        const version = if (root.object.get("version")) |version| version.string else return error.NoVersionInManifest;
+
+        std.debug.print("{s}: {s}\n", .{ extension_name, version });
+    }
+}
+
+fn lookupLocaleName(
+    allocator: std.mem.Allocator,
+    dest: std.fs.Dir,
+    locale: []const u8,
+    placeholder: []const u8,
+) !?[]const u8 {
+    const locale_file = try dest.openFile(locale, .{});
+    defer locale_file.close();
+
+    const root = try parseJsonFile(allocator, locale_file);
+
+    var it = std.mem.tokenizeScalar(u8, placeholder, '_');
+    _ = it.next();
+
+    const key = it.next() orelse return null;
+    const value = root.object.get(key) orelse return null;
+
+    const message = value.object.get("message") orelse return null;
+    return message.string;
+}
+
+fn parseJsonFile(allocator: std.mem.Allocator, file: std.fs.File) !std.json.Value {
     var file_buffer: [1024]u8 = undefined;
-    var file_reader = manifest.reader(&file_buffer);
+    var file_reader = file.reader(&file_buffer);
     const reader = &file_reader.interface;
 
     var json_reader = std.json.Reader.init(allocator, reader);
     const parsed = try std.json.parseFromTokenSource(std.json.Value, allocator, &json_reader, .{});
 
-    const root = parsed.value;
-    if (root.object.get("version")) |version| {
-        std.debug.print("{s}\n", .{version.string});
-    }
+    return parsed.value;
 }
 
-fn getChromeVersion(raw_version: []const u8) []const u8 {
+fn extractManifestAndLocale(
+    allocator: std.mem.Allocator,
+    reader: *std.fs.File.Reader,
+    dest: std.fs.Dir,
+) !?[]const u8 {
+    var it: std.zip.Iterator = try .init(reader);
+    var filename_buffer: [std.fs.max_path_bytes]u8 = undefined;
+    var found_manifest = false;
+    var locale: ?[]const u8 = null;
+    while (try it.next()) |entry| {
+        const filename = filename_buffer[0..entry.filename_len];
+        try reader.seekTo(entry.header_zip_offset + @sizeOf(std.zip.CentralDirectoryFileHeader));
+        try reader.interface.readSliceAll(filename);
+        if (std.mem.eql(u8, filename, manifest_json)) {
+            try entry.extract(reader, .{}, &filename_buffer, dest);
+            found_manifest = true;
+        } else if (std.mem.startsWith(u8, filename, "_locales/en") and
+            std.mem.endsWith(u8, filename, "messages.json"))
+        {
+            locale = try allocator.dupe(u8, filename);
+            try entry.extract(reader, .{}, &filename_buffer, dest);
+        }
+
+        if (found_manifest and locale != null) break;
+    }
+
+    if (!found_manifest) return error.NoManifestInArchive;
+
+    return locale;
+}
+
+fn getChromeVersion(allocator: std.mem.Allocator) ![]const u8 {
+    const chrome_result = try std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = &.{ "chromium", "--version" },
+    });
+
+    const raw_version = chrome_result.stdout;
     const chrome_str = std.mem.sliceTo(raw_version, ' ');
     const version_str = raw_version[chrome_str.len + 1 ..];
     const version = std.mem.sliceTo(version_str, '.');
